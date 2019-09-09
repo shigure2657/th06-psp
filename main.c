82b7d59c3d1d5040ec1182cef47076fe520d337b
cocoai2657 <cloudcompute2657@gmail.com>
2019-09-09 12:26:04 +0900

    Update

--- main.c
+++ main.c
@@ -3,16 +3,30 @@
 PSP_MODULE_INFO("DxLib_LBL", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
 
+void initial_logo()
+{
+    int bg;
+    int t;
+    bg = LoadGraph("./img/th06logo.png");
+    t = GetNowCount();
+
+    while (GetNowCount() - t < 5000) {
+         ClearDrawScreen();
+         DrawGraph(0,0,bg,FALSE);
+         ScreenFlip();
+    }
+}
 
 int main()
 {
-    int bg;
     if(DxLib_Init() == -1)return -1;
-    bg = LoadGraph("./img/th06logo.png");
-    while(ProcessMessage() != -1)
-    {
+    initial_logo();
+
+    int menu_bg;
+    menu_bg = LoadGraph("./img/title00.png");
+    while (ProcessMessage() != -1) {
         ClearDrawScreen();
-        DrawGraph(0,0,bg,FALSE);
+        DrawGraph(58,0,menu_bg,FALSE);
         ScreenFlip();
     }
     DxLib_End();
